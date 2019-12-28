2344eef0bf4b72b99fdebf9d7a51c7100892fa4b
Derek <derek.tofi@gmail.com>
2019-12-28 14:10:57 +0000

    Add new general account model

--- models/src/main/java/com/crewly/models/account/Account.kt
+++ models/src/main/java/com/crewly/models/account/Account.kt
@@ -1,4 +1,19 @@
 package com.crewly.models.account
 
-class Account {
-}
+import com.crewly.models.Company
+import com.crewly.models.Rank
+import com.crewly.models.Salary
+import org.joda.time.DateTime
+
+data class Account(
+  val crewCode: String = "",
+  var name: String = "",
+  var company: Company = Company.None,
+  var base: String = "",
+  val rank: Rank = Rank.NONE,
+  var isPilot: Boolean = false,
+  val joinedCompanyAt: DateTime = DateTime(0),
+  val showCrew: Boolean = true,
+  val updateSectorsRealTimeEnabled: Boolean = false,
+  val salary: Salary = Salary()
+)
